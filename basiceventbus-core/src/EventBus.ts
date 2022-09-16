88fa45b8a5a1a8a3cbd8dd673e7002d9465afa67
adam goldsmith <adam.goldsmith0@gmail.com>
2022-09-16 13:02:45 +0100

    conforming to TS naming conventions

--- basiceventbus-core/src/EventBus.ts
+++ basiceventbus-core/src/EventBus.ts
@@ -8,22 +8,22 @@ class EventBus{
         return EventBus._instance ?? (EventBus._instance = new EventBus());
     }
 
-    Subscribe(eventName: string, func: Function) {
+    subscribe(eventName: string, func: Function) {
         if(this.checkDuplicates(eventName, func)){
             this.events.push(new EventBinder(eventName, func));
         }
     };
 
-    Unsubscribe(eventName: string, func: Function) {
+    unsubscribe(eventName: string, func: Function) {
         this.events.splice(this.events.findIndex(e => e.Name == eventName && e.Function == func), 1);
     };
 
-    Emit(eventName: string, data: any) {
+    emit(eventName: string, data: any) {
         let matchingEvents = this.events.filter(e => e.Name == eventName);
         matchingEvents.forEach(e => e.Function(data));
     };
 
-    Reset(){
+    reset(){
         this.events = [];
     };
 
