b94f7840e4d11fc32fd6b5893820672f3c64d831
Adam Goldsmith <adam.goldsmith0@gmail.com>
2023-05-26 12:26:30 +0100

    naming the json output of jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -17,5 +17,8 @@ export default {
     '\\.pnp\\.[^\\\\]+$'
   ],
   collectCoverage: true,
-  coverageReporters: ['json']
+  coverageReporters: ['json', ['json',{ file: 'coverage-summary.json' }]],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/tests/testClasses'
+  ]
 };
