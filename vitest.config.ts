fa415b3b27e806cf274c4be61f87ab8f12528e4d
Adam Goldsmith <62713267+AdamNJG@users.noreply.github.com>
2025-09-08 14:29:42 +0100

    Adding docgen (#9)
    * adding docGen
    
    * no!
    
    * updating dependencies, more updates to docs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ['index.ts','eslint.config.ts', 'vitest.config.ts', 'tests/**']
+      exclude: ['index.ts','eslint.config.ts', 'vitest.config.ts', 'tests/**', 'docgen.config.ts']
     }
   }
 });
